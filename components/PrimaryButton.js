c7624d1bcb33770d2eb2e13a6647dbfd83574eb2
Yanters <yanter@wp.pl>
2022-12-25 20:54:37 +0100

    input element

---
+++ components/PrimaryButton.js
@@ -0,0 +1,46 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+const PrimaryButton = ({ children }) => {
+  const onPress = () => {
+    console.log('PrimaryButton pressed');
+  };
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.buttonInnerContainer,
+          pressed && styles.pressed,
+        ]}
+        onPress={onPress}
+        android_ripple={{ color: '#640233' }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    // Android shadow
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,64 @@
+import { View, TextInput, StyleSheet } from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType='number-pad'
+        autoCapitalize='none'
+        autoCorrect={false}
+        placeholder='00'
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    // flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 16,
+    backgroundColor: '#4e0329',
+    borderRadius: 8,
+    // Android shadow
+    elevation: 8,
+    // IOS shadow
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    margiinVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
